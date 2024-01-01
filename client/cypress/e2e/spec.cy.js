393cf24d7929017e76b0d8299fbd61d4f3fc13b5
Mauro Moyano <mauromoyanodev@gmail.com>
2024-01-01 03:37:35 -0300

    cypress running

---
+++ client/cypress/e2e/spec.cy.js
@@ -0,0 +1,25 @@
+
+
+// describe('template spec', () => {
+//   it('passes', () => {
+//     cy.visit('https://localhost:3000');
+//   })
+// })
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+describe('My First Test', () => {
+  it('Does not do much!', () => {
+    expect(true).to.equal(true)
+  })
+})
+
+describe('My First Test in App', () => {
+  it('Visits the pokemon PI', () => {
+    cy.visit('http://localhost:3000') 
+  })
+})
