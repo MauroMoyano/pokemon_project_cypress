cdb12c560343a9cc7a2992392ba7cbfa40fffc57
Mauro Moyano <mauromoyanodev@gmail.com>
2024-01-04 23:40:26 -0300

    working in tests

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-
+/// <reference types="Cypress" />
 
 // describe('template spec', () => {
 //   it('passes', () => {
@@ -21,5 +21,44 @@ describe('My First Test', () => {
 describe('My First Test in App', () => {
   it('Visits the pokemon PI', () => {
     cy.visit('http://localhost:3000') 
+    cy.contains('bulbasaur')
+  })
+})
+
+describe('My First Test', () => {
+  it('clicks the link "type"', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+  })
+})
+
+describe('My First Test /commands', () => {
+  it('clicking "type" navigates to a new url', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+  })
+})
+
+describe('My First Test', () => {
+  it('Gets, types and asserts', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it
+    cy.get('.action-email').type('fake@email.com')
+
+    //  Verify that the value has been updated
+    cy.get('.action-email').should('have.value', 'fake@email.com')
   })
 })
